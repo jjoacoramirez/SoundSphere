67ddb7f9e6a03ec150496f00d114b0484d430798
mateogalli <mateogalli@hotmail.com>
2024-11-07 14:04:56 -0300

    probando js

---
+++ carrusel.js
@@ -0,0 +1,8 @@
+document.getElementById("flecha").addEventListener("click",funcion1)
+document.getElementById("flecha").addEventListener("mouseover",funcion2)
+
+function funcion1(){
+    alert("proximamente carrusel")
+}
+function funcion2(){
+}
