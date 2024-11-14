95681e39c9fed80b83f43868d5fd57e5674bbc4c
mateogalli <mateogalli@hotmail.com>
2024-11-14 16:32:02 -0300

    Correcciones

--- scripts.js
+++ scripts.js
@@ -5,14 +5,16 @@ let volumenControl = document.getElementById("volumen")
 let progressTime = document.getElementById("progressTime")
 let prevBtn = document.getElementById("volver");
 let nextBtn = document.getElementById("siguiente");
+let flechaPrev = document.getElementById("anterior");
+let flechaNext = document.getElementById("proximo");
 let cancionActualIndice = 0;
-
+ 
 let canciones = [
     { src: "audio/colors-video.mp3", titulo: "Coloros", artista: "Crossfade", portada: "img/colors.png" },
     { src: "audio/covet-official-visualizer.mp3", titulo: "Covet", artista: "Basement", portada: "img/Covet.png" },
     { src: "audio/diamond-eyes-official-lyric-video.mp3", titulo: "Diamond Eyes", artista: "Deftones", portada: "img/Diamond Eyes.png" },
 ];
-
+ 
 function playPause(){
     if(ctrl.classList.contains("pause")){
         cancion.pause();
@@ -24,61 +26,62 @@ function playPause(){
         ctrl.classList.add("pause");
     }
 }
-
+ 
 function formatTime(seconds) {
     const minutes = Math.floor(seconds / 60);
     const secs = Math.floor(seconds % 60).toString().padStart(2, '0');
     return `${minutes}:${secs}`;
 }
-
+ 
 function loadSong(cancionIndice) {
     cancion.src = canciones[cancionIndice].src;
     cancion.play();
     ctrl.classList.remove("play");
     ctrl.classList.add("pause");
-    
+   
     document.getElementById("portada").src = canciones[cancionIndice].portada;
     document.getElementById("nombreMusica").textContent = canciones[cancionIndice].titulo;
     document.getElementById("nombreCantante").textContent = canciones[cancionIndice].artista;
 }
-
+ 
 function previousSong() {
     cancionActualIndice = (cancionActualIndice === 0) ? canciones.length - 1 : cancionActualIndice - 1;
     loadSong(cancionActualIndice);
 }
-
+ 
 function nextSong() {
     cancionActualIndice = (cancionActualIndice === canciones.length - 1) ? 0 : cancionActualIndice + 1;
     loadSong(cancionActualIndice);
 }
-
+ 
 cancion.onloadedmetadata = function(){
     progress.max = cancion.duration;
     progress.value = cancion.currentTime;
     }
-
+ 
 volumenControl.addEventListener("input", () => {
     const volumen = volumenControl.value;
     cancion.volume = volumen / 100;
 })
-
+ 
 progress.addEventListener("input", () => {
     const tiempo = progress.value;
     cancion.currentTime = tiempo;
 })
-
+ 
 cancion.addEventListener("timeupdate", () => {
     const currentTime = cancion.currentTime;
     const duration = cancion.duration;
     progress.value = (currentTime / duration) * 100;
     progressTime.textContent = `${formatTime(currentTime)} / ${formatTime(duration)}`;
 });
-
+ 
 cancion.ontimeupdate = function(){
     progress.value = cancion.currentTime;
 }
-
+ 
 prevBtn.addEventListener("click", previousSong);
 nextBtn.addEventListener("click", nextSong);
-
+ 
 loadSong(cancionActualIndice);
+ 
